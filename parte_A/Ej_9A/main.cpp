83cb7e2efdde0201f7d3ecb210bc8a867af0e1f2
SebaRF89 <108742285+SebaRF89@users.noreply.github.com>
2024-04-17 00:39:26 -0300

    Add files via upload

---
+++ parte_A/Ej_9A/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	int vector[100], contador = 0;
+	
+	for(int i=0; i<100; i++){
+		vector[i] = i;
+	}
+	
+	for(int i=1; i<100; i++){
+		if(vector[i] <= vector[i-1]){
+			contador += 1;
+		}
+	}
+		
+	cout<<"¿El vector esta ordenado ascendentemente?"<<contador;	
+
+	if(contador>0){
+		cout<<"\nNO";
+	}else{
+		cout<<"\nSI";
+
+	}
+		
+	return 0;
+}
